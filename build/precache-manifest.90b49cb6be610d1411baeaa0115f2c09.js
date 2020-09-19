c9da847e963be42ac385f5162e582e9601d27338
OINDRILA GHOSH <oindrila.ghosh@capitalnumbers.com>
2020-09-19 07:20:01 -0700

    build folder

---
+++ build/precache-manifest.90b49cb6be610d1411baeaa0115f2c09.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2970af883ea71d0f7e1426a7475e2abf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0f4b77f8ea1205a29a6",
+    "url": "/static/css/main.9d8d1da5.chunk.css"
+  },
+  {
+    "revision": "124793f3cdbb4208e65e",
+    "url": "/static/js/2.a74b8861.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/static/js/2.a74b8861.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0f4b77f8ea1205a29a6",
+    "url": "/static/js/main.929d1532.chunk.js"
+  },
+  {
+    "revision": "0f141f1a15ea4bbbb834",
+    "url": "/static/js/runtime-main.ca98175e.js"
+  },
+  {
+    "revision": "0c584575cc6c5594433ecf8ae8ee8c29",
+    "url": "/static/media/covid3.0c584575.jpg"
+  }
+]);
